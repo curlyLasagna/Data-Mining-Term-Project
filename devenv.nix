00c85dd3a39337611093387f32e4cb2f92b71a66
luis <luis.gcodes@gmail.com>
2025-12-03 13:21:49 -0500

    add devenv to manage deps

---
+++ devenv.nix
@@ -0,0 +1,48 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    typst
+    tinymist
+    gyre-fonts
+  ];
+  languages.python = {
+    enable = true;
+    version = "3.14.0";
+    venv.enable = true;
+
+    uv = {
+      enable = true;
+      sync.enable = true;
+      sync.allExtras = true;
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
